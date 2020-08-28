eb34e95d7bc53ff38fe2b33d4688f42e4741b2e4
YHideki <djalmahy@hotmail.com>
2020-08-28 17:24:13 -0300

    criação do arquivo organização-basica.js

---
+++ fundamentos/organizacao-basica.js
@@ -0,0 +1,8 @@
+console.log("Ola Mundo! Eu sou uma sentença em javascript")
+
+{
+    {
+        console.log("Ola Mundo! Eu sou um bloco de código")
+        console.log("Ola Mundo! E eu sou outro bloco de código javascript")
+    }
+}
